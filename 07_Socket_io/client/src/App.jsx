39be1eb4e6d26da3e6ae47925a4c27107f688678
ahmi5567 <ah55677435@gmail.com>
2025-10-26 11:42:38 +0500

    tiny url server and socket io implementation

---
+++ 07_Socket_io/client/src/App.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import { io } from "socket.io-client";
+import JoinGroup from "./components/JoinGroup";
+import ChatRoom from "./components/ChatRoom";
+
+const SOCKET_URL = "http://localhost:8000";
+let socket;
+function App() {
+  const [userInfo, setUserInfo] = useState({ username: "", room: "" });
+  const [joined, setJoined] = useState();
+
+  useEffect(() => {
+    socket = io(SOCKET_URL);
+
+    socket.on("connect", () => {
+      console.log("connected to server");
+    });
+    socket.on("disconnect", () => {
+      console.log("Disconnected from server");
+    });
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  const handleJoin = ({ username, room }) => {
+    setUserInfo({ username, room });
+    if (socket) {
+      socket.emit("join", room);
+    }
+    setJoined(true);
+  };
+
+  const handleLeave = () => {
+    setUserInfo({ username: "", room: "" });
+    setJoined(false);
+  };
+
+  return (
+    <>
+      {!joined ? (
+        <JoinGroup onJoin={handleJoin} />
+      ) : (
+        <ChatRoom
+          username={userInfo.username}
+          room={userInfo.room}
+          socket={socket}
+          onLeave={handleLeave}
+        />
+      )}
+    </>
+  );
+}
+
+export default App;
---
+++ 07_Socket_io/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: "*",
+    methods: ["POST", "GET"],
+    credentials: true,
+  },
+});
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the realtime chatapp");
+});
+
+io.on("connection", (socket) => {
+  console.log("user connected", socket.id);
+  // join and leave room
+  socket.on("join", (roomId) => {
+    socket.join(roomId);
+  });
+  socket.on("leave", (roomId) => {
+    socket.leave(roomId);
+  });
+
+  socket.on("send", (message) => {
+    console.log("Message received:", message);
+    io.to(message.room).emit("message", message); // sabhi ko milega
+  });
+});
+
+server.listen(8000, () => {
+  console.log("Server connected");
+});
